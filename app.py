326401fb8fb95a81005f9665fda0a16a7be2106c
The0ven <ovencock@gmail.com>
2024-09-08 22:25:56 -0400

    fixed not printing bug

--- app.py
+++ app.py
@@ -62,8 +62,9 @@ async def new_year():
     if os.path.exists('history.jsonl'):
         df = pd.read_json('history.jsonl', lines=True)
         delta = df['timestamp'].iat[-1] - dt.fromtimestamp(time())
+        sy_delta = df.dropna(subset=["printed"])['timestamp'].iat[-1] - dt.fromtimestamp(time())
         print(f"{df['timestamp'].iat[-1]}, time_since_last: {abs(delta.total_seconds() / 60):.2f}, delta_threshold_sy: {timedelta(hours=sy['hours_per_year']).total_seconds() / 60:.2f}, delta_threshold_min: {timedelta(hours=6).total_seconds() / 60:.2f}")
-        if abs(delta) > timedelta(hours=sy["hours_per_year"]):
+        if abs(sy_delta) > timedelta(hours=sy["hours_per_year"]):
             entry = compute_years(df.to_dict("records")[-1], config['calendars'])
             df.loc[len(df.index)] = entry
         elif abs(delta) > timedelta(hours=6):
